2db89858f1937d8b879acdef7dbec6c52a5c8479
blokkergang <nanninjago@gmail.com>
2021-06-27 10:24:53 +0200

    ini

--- commands/ip.js
+++ commands/ip.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) =>{
 
-    return message.channel.send("play.blokhub.eu op 1.16 of hoger");
+    return message.channel.send("cooming soon");
 
 }
 
--- commands/rank.js
+++ commands/rank.js
@@ -7,5 +7,5 @@ module.exports.run = async(bot, message, args) =>{
 }
 
 module.exports.help = {
-    name: "rank"
+    name: "rankinfo"
 }
