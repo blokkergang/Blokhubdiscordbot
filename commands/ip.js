0d0363e86fcb71ba78ecf3eeffbac06caeb3cbf9
blokkergang <nanninjago@gmail.com>
2021-06-29 16:57:39 +0200

    Update ip.js

--- commands/ip.js
+++ commands/ip.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) =>{
 
-    return message.channel.send("cooming soon");
+    return message.channel.send("play.blokhub.eu op 1.16.4 t/m 1.17 maar we raden voor survival 1.17 aan en de rest 1.16.4.");
 
 }
 
